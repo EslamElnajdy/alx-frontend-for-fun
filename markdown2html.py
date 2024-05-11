3c1ccfefe89c389e75e04fc038c55d8a17481e7c
Eslam <114030950+EslamElnajdy@users.noreply.github.com>
2024-05-11 09:54:51 +0000

    prevent the file excuted when imported

--- markdown2html.py
+++ markdown2html.py
@@ -10,13 +10,17 @@ script markdown2html.py that takes an argument 2 strings:
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
 
-markdown_file = sys.argv[1]
-if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
+    markdown_file = sys.argv[1]
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
 
-sys.exit(0)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
