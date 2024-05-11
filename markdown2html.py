4111677c901cb61152e3c1ab5cfb3755b1f41de2
Eslam <114030950+EslamElnajdy@users.noreply.github.com>
2024-05-11 09:51:54 +0000

    initial commit

---
+++ markdown2html.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+script markdown2html.py that takes an argument 2 strings:
+    - First argument is the name of the Markdown file
+    - Second argument is the output file name
+"""
+
+
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
+
+markdown_file = sys.argv[1]
+if not os.path.exists(markdown_file):
+    print(f"Missing {markdown_file}", file=sys.stderr)
+    sys.exit(1)
+
+sys.exit(0)
